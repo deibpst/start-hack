5790b10c748aeb669ba4ea84c2359b10ec8419e5
deib <id1386859@gmail.com>
2026-02-01 11:11:58 -0600

    Vite config, supabase fix y azules

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     allowedHosts: true,
     hmr: {
